3e1f8d713f5ab25123bdfaf52d6d2cf78f8c0234
Oksana <ksenia79460@gmail.com>
2023-02-06 14:22:10 +0000

    use state, fetch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
+import React, { useState } from "react";
+
 import { FaTwitter, FaQuoteLeft } from "react-icons/fa";
 
 import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const getQuotes = () => {
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log(data);
+
+        const allDataQuotes = data.quotes;
+        const randomNum = Math.floor(Math.random() * allDataQuotes.length);
+        const randomQuote = allDataQuotes[randomNum];
+        console.log(randomQuote);
+
+        setQuote(randomQuote.quote);
+        setAuthor(randomQuote.author);
+      })
+      .catch((err) => console.error("Sorry"));
+  };
+
   return (
     <div className="App">
       <div id="quote-box">
         <div id="quote-text">
-          <FaQuoteLeft style={{
-            fontSize: "1em",
-            marginRight: "0.4em",
-            display: "inline-block",
-            verticalAlign: "middle",
-          }} />
-          <span id="text"></span>
+          <FaQuoteLeft
+            style={{
+              fontSize: "1em",
+              marginRight: "0.4em",
+              display: "inline-block",
+              verticalAlign: "middle",
+            }}
+          />
+          <span id="text">{quote}</span>
         </div>
-        <div id="author">-</div>
+        <div id="author">- {author}</div>
         <div className="buttons">
           <a
             href="twitter.com/intent/tweet"
@@ -30,10 +56,8 @@ function App() {
                 backgroundColor: "black",
                 float: "left",
                 padding: "6px 0",
-               
                 fontSize: "inherit",
                 textAlign: "center",
-                
                 width: "40px",
                 height: "38px",
                 border: "none",
@@ -48,12 +72,13 @@ function App() {
             />
           </a>
 
-          <button className="button" id="new-quote">
+          <button className="button" id="new-quote" onClick={() => getQuotes()}>
             New quote
           </button>
         </div>
       </div>
-      <div className="footer">by 
+      <div className="footer">
+        by
         <a href="/"> Oksana</a>
       </div>
     </div>
